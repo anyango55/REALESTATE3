f912cb5e8c6ad7071463f1a423e035808fe8ac84
anyango55 <anyangoc55@gmail.com>
2023-10-24 13:57:12 +0300

    connct to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 // install exprees to create backend/APIs
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+    console.log('connected to mongodb');
+})
+.catch((err) => {
+    console.log(err);
+});
+
+// INSTALL DOTENV TO USE ENVIRONMENT VARIABLES , because you cant use environmental variables in node.js by default
 
 const app = express();
 
